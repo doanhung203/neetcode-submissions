550ae660c63cf0994ca265b296789570cb106c1b
Hung Doan <manhhungdoan203@gmail.com>
2026-05-22 23:20:34 +0700

    Add: find-pivot-index - submission-1

---
+++ Data Structures & Algorithms/find-pivot-index/submission-1.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int PivotIndex(int[] nums) {
+        int total = 0;
+        foreach(int num in nums){
+            total += num;
+        }
+        int leftSum = 0;
+        for(int i = 0; i < nums.Length; i++){
+            int rightSum = total - nums[i] - leftSum;
+            if(leftSum == rightSum) return i;
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+}
