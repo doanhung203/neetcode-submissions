fff0fa00a831cd6beba1024b0f40b21a8c542959
Hung Doan <manhhungdoan203@gmail.com>
2026-05-31 22:45:57 +0700

    Add: pascals-triangle - submission-0

---
+++ Data Structures & Algorithms/pascals-triangle/submission-0.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public List<List<int>> Generate(int numRows) {
+        var result = new List<List<int>>();
+        for(int i = 0; i < numRows; i++){
+            var row = new List<int>();
+            for(int j = 0; j <= i; j++){
+                if(j == 0 || j == i){
+                    row.Add(1);
+                }
+                else{
+                    row.Add(result[i - 1][j - 1] + result[i - 1][j]);
+                }
+            }
+            result.Add(row);
+        }
+        return result;
+    }
+}
